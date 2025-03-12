ed65a8098036950c1ac273b0fd35263f0ef2dd7a
victor1206 <victor_rivas89@hotmail.com>
2025-03-11 20:05:07 -0600

    Agregar archivos de proyecto.

---
+++ Practica20250311G06.Dbfirst/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Practica20250311G06.Dbfirst.Models;
+using System.Diagnostics;
+
+namespace Practica20250311G06.Dbfirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Practica20250311G06.Dbfirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica20250311G06.Dbfirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
